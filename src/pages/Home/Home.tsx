892aac0710a7927c077e56a0c677f4465f59d79f
“Vladislav <boromirchik@gmail.com>
2025-07-26 15:00:42 +0300

    refactor: memoize preview filtering in Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,29 +8,47 @@ import { useSerialPreviews } from '../../hooks/useSerialPreviews'
 import { useChannelPreviews } from '../../hooks/useChannelPreviews'
 
 import styles from './Home.module.css'
-
+import { useMemo } from 'react'
 
 const Home = () => {
-
 	// Делаю запросы для получения превью фильмов и сериалов
 	const { data: dataMoviesPrev = [], isError: movieIsError, error: movieError } = useMoviePreviews()
 	const { data: dataSerialsPrev = [], isError: serialIsError, error: serialError } = useSerialPreviews()
 	const { data: dataChanelPrev = [], isError: chanelIsError, error: chanelError } = useChannelPreviews()
 
-	// Фильтрую данные для получения новых и популярных фильмов и серилов
-	const newMovies = dataMoviesPrev.filter((movie: MoviePreviews) => movie.is_new)
-	const popularMovies = dataMoviesPrev.filter((movie: MoviePreviews) => movie.is_popular)
-	const newSerials = dataSerialsPrev.filter((serial: SerialPreviews) => serial.is_new)
-	const popularSerials = dataSerialsPrev.filter((serial: SerialPreviews) => serial.is_popular)
-	const newChanels = dataChanelPrev.filter((chanel: ChanelPreview) => chanel.is_new)
-	const popularChanels = dataChanelPrev.filter((chanel: ChanelPreview) => chanel.is_recommended)
+	// Фильтрую и мемоизирую данные для новых и популярных фильмов, сериалов и каналов
+	const { newMovies, popularMovies } = useMemo(
+		() => ({
+			newMovies: dataMoviesPrev.filter((movie: MoviePreviews) => movie.is_new),
+			popularMovies: dataMoviesPrev.filter((movie: MoviePreviews) => movie.is_popular)
+		}),
+		[dataMoviesPrev]
+	)
 
+	const { newSerials, popularSerials } = useMemo(
+		() => ({
+			newSerials: dataSerialsPrev.filter((serial: SerialPreviews) => serial.is_new),
+			popularSerials: dataSerialsPrev.filter((serial: SerialPreviews) => serial.is_popular)
+		}),
+		[dataSerialsPrev]
+	)
 
+	const { newChanels, popularChanels } = useMemo(
+		() => ({
+			newChanels: dataChanelPrev.filter((chanel: ChanelPreview) => chanel.is_new),
+			popularChanels: dataChanelPrev.filter((chanel: ChanelPreview) => chanel.is_recommended)
+		}),
+		[dataChanelPrev]
+	)
 
 	// Блок с ошибкой
-	const movieErrorBlock = <div className={styles.error}>{movieError?.message ?? <p>Произошла ошибка</p>}</div>
-	const serialErrorBlock = <div className={styles.error}>{serialError?.message ?? <p>Произошла ошибка</p>}</div>
-	const chanelErrorBlock = <div className={styles.error}>{chanelError?.message ?? <p>Произошла ошибка</p>}</div>
+	const movieErrorBlock = (
+		<div className={styles.error}>{movieError?.message ?? <p>Произошла ошибка загрузки фильмов</p>}</div>
+	)
+	const serialErrorBlock = (
+		<div className={styles.error}>{serialError?.message ?? <p>Произошла ошибка загрузки сериалов</p>}</div>
+	)
+	const chanelErrorBlock = <div className={styles.error}>{chanelError?.message ?? <p>Произошла ошибка каналов</p>}</div>
 
 	console.log(newChanels, popularChanels, chanelErrorBlock, chanelIsError)
 
@@ -38,33 +56,25 @@ const Home = () => {
 		<>
 			<Slider />
 			<div className={styles.videoWrap}>
-				{!movieIsError && newMovies.length > 0 ? (
-					<VideoList title='new movies' type='movies' url='#' data={newMovies} />
-				) : (
-					movieErrorBlock
-				)}
+				{movieIsError
+					? movieErrorBlock
+					: newMovies.length > 0 && <VideoList title='new movies' type='movies' url='#' data={newMovies} />}
 
-				{!movieIsError && popularMovies.length > 0 ? (
-					<VideoList title='popular movies' type='movies' url='#' data={popularMovies} />
-				) : (
-					movieErrorBlock
-				)}
+				{movieIsError
+					? movieErrorBlock
+					: popularMovies.length > 0 && <VideoList title='popular movies' type='movies' url='#' data={popularMovies} />}
 
-				{!serialIsError && newSerials.length > 0 ? (
-					<VideoList title='new serials' type='serials' url='#' data={newSerials} />
-				) : (
-					serialErrorBlock
-				)}
+				{serialIsError
+					? serialErrorBlock
+					: newSerials.length > 0 && <VideoList title='new serials' type='serials' url='#' data={newSerials} />}
 
-				{!serialIsError && popularSerials.length > 0 ? (
-					<VideoList title='popular serials' type='serials' url='#' data={popularSerials} />
-				) : (
-					serialErrorBlock
-				)}
+				{serialIsError
+					? serialErrorBlock
+					: popularSerials.length > 0 && (
+							<VideoList title='popular serials' type='serials' url='#' data={popularSerials} />
+					  )}
 
-				{/* <VideoList title='new serials' type='movies' url='#' />
-
-				<VideoList title='channel' type='channels' url='#' /> */}
+				{/* <VideoList title='channel' type='channels' url='#' /> */}
 			</div>
 		</>
 	)
